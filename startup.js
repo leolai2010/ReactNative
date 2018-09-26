d01634fbbb4e7bd0c794379dfefe88c26428a469
Leo <leolai2010@gmail.com>
2018-09-26 03:55:10 -0700

    successfully emulation of android app

--- startup.js
+++ startup.js
@@ -1,1 +1,16 @@
 //install Java SE Development Kit
+//install Node.js
+//install Python 2 
+//install Android Stuido
+//in command
+npm install -g react-native-cli
+react-native init <project name>
+//Use Android Studio to run a emulation of phone - Open project folder amd select android
+//In advanced system settings add new Environmental variable
+// Name: JAVA_HOME
+// Value: C:\Program Files\Java\jdk1.8.0_181
+// edit Path to add C:\Users\Leo Lai\AppData\Local\Android\sdk\platform-tools
+// reopen new terminal and navigate to project folder
+npm add @babel/runtime
+react-native run-android
+// for dev menu debug press ctrl + M
