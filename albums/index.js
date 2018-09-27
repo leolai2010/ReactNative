db1725e7f9b6bef8e9fa82d80e8f9ce6af63a996
Leo <leolai2010@gmail.com>
2018-09-27 02:40:04 -0700

    start some basic

--- albums/index.js
+++ albums/index.js
@@ -1,5 +1,17 @@
 /** @format */
 
+//Import a library to help create a component
+import React from 'react'; //how component behaves 
+import ReactNative from 'react-native'; //how it should behave in mobile devices
+//Create a component
+const App = () => {
+    return (
+        <Text>Some Text</Text>
+    );
+};
+
+//Render it to the device
+
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
